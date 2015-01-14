78d91dd7eae9f4f20b47122cbe9c588e6362a3b5
Henry Phuong <henry.phuong@distrix.com>
2015-01-14 14:34:08 -0800

    Added stack and buffer features

---
+++ buffer/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 14, 2015
+ *      Author: l
+ */
+
+#include <iostream>
+#include "stack.h"
+int main () {
+
+	std::cout << "Hello Henry" << std::endl;
+}
+
+
